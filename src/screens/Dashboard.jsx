c5e5cd00b32acd9c995a0dbef1550f9108f0e00a
mahlaule <kevinmahlaule@gmail.com>
2022-09-30 11:10:38 +0200

    login added

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import Header from '../components/Header'
+import React, {useEffect, useState} from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import Header from '../components/Header';
 
-const Dashboard = () => {
+import { auth } from '../config/firebase';
+import { signOut } from 'firebase/auth';;
+
+const Dashboard = ({navigation}) => {
+//states for user email
+const [email, setEmail] = useState('');
+
+useEffect(()=>{
+  if (auth.currentUser) {
+    setEmail(auth.currentUser.email);
+  } else {
+    navigation.navigate('Login');
+  }
+}, []);
+
+const logout = async() => {
+  await signOut(auth).then(
+    ()=> {
+      navigation.navigate('Login');
+    }
+  )
+}
+ 
   return (
     <View style={styles.container}>
         <Header title='Home' />
+        <Text>{email}</Text>
+        <Button title='Logout' onPress={logout} />
         <Text>Welcome to the app!</Text>
     </View>
   )
