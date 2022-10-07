c78251bc4b9914b45aedb02ee8d2992abe0fe3e6
unknown <msenyanyathi33@gmail.com>
2022-10-07 16:39:21 +0200

    update

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import Login from './src/screens/Login';
 import Register from './src/screens/Register';
 import Dashboard from './src/screens/Dashboard';
 import Splash from './src/screens/Splash'
+// import Profile from './src/screens/Profile';
+
 
 import { auth } from './src/config/firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -15,14 +17,21 @@ import { useState } from 'react';
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
   //function to register new user with email and password
   //function to login user with email and password
   //function to log out the user
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Splash'>
-        <Stack.Screen name='Splash' options={{title:'splash'}}>
+    <NavigationContainer >
+      <Stack.Navigator  screenOptions={{
+        headerStyle:{
+          backgroundColor:'#96DED1'
+        }
+      
+      }}  >
+        <Stack.Screen name='dd' options={{title:'' }}>
           {(props) => <Splash {...props} />}
         </Stack.Screen>
         <Stack.Screen name='Login' options={{title:'Login'}}>
@@ -31,11 +40,19 @@ export default function App() {
 
         <Stack.Screen name='Register' options={{title:'Register'}}>
           {(props) => <Register {...props} />}
-        </Stack.Screen>
+        </Stack.Screen> 
 
         <Stack.Screen name='Home' options={{title:'Home'}}>
           {(props) => <Dashboard {...props} />}
-        </Stack.Screen>
+        </Stack.Screen> 
+
+        {/* <Stack.Screen name='Profile' options={{title:'Profile'}}>
+          {(props) => <Profile {...props} />}
+        </Stack.Screen>  */}
+
+       
+
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -47,5 +64,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    maxHeight:840,
+    maxWidth:1000,
+    height:896,
+    width:424,
   },
 });
